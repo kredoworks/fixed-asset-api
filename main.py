903e43de5970ee6e6998f82760d31b222dd8cde5
chaitanyaKredo <98405183+chaitanyaKredo@users.noreply.github.com>
2025-12-11 11:33:53 +0530

    working version with cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.cycles.views import router as cycles_router
 from api.verification.views import router as verification_router
@@ -14,6 +15,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Fixed Asset API",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(cycles_router, prefix="/api/v1")
